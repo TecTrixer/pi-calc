6eb8d8b13f5a96a3f50b27f81ebefc10f125b7f1
TecTrixer <tonihoevedes@gmail.com>
2021-03-30 00:53:08 +0200

    added python web scraper for pi using math.tools

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-from math import sqrt
-def area_of_triangle(a, b):
-    return 0.5 * a * b
+# import necessary modules
+import requests, bs4, os
 
-def b_from_a(a):
-    return 1 - sqrt(1-a*a)
+# request
+res = requests.get('https://math.tools/numbers/pi/10')
 
-def new_a(previous_a, previous_b):
-    return 0.5 * sqrt(previous_a*previous_a + previous_b*previous_b)
+# gain all html-code
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-def pi_from_area(area):
-    return area * 4
+# CSS-selector
+Elems = soup.find('div', class_='pidata')
 
-area = 0.5
-step = 1
-a = sqrt(2)*0.5
-while True:
-    b = b_from_a(a)
-    area += area_of_triangle(a, b) * 2**step
-    step += 1
-    a = new_a(a, b)
-    print("{:.50f}".format(pi_from_area(area)), step)
+for elem in Elems.contents:
+    print(elem[1:-1])
